14131c268453c1931191fdf08e5835e85b8821ce
shuqing huang <147402275+Hsqcsu@users.noreply.github.com>
2026-01-31 18:05:49 +0800

    Update Retrieval.py

--- CSU-IR/100-Million-library-Retrieval/Retrieval.py
+++ CSU-IR/100-Million-library-Retrieval/Retrieval.py
@@ -17,6 +17,8 @@ import gc
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors
 from collections import defaultdict
+import pandas as pd
+import jcamp
 
 # --- 1. Set up the project root directory, which is the base for all relative paths ---
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -28,7 +30,15 @@ sys.path.insert(0, PROJECT_ROOT)
 from model.IR_encoder import IRModel
 from model.SMILES_encoder import SmilesModel
 
+from data_process.ir_process import preprocess_absorbances_spectra_higer_500
+from data_process.ir_process import preprocess_absorbances_spectra_lower_500
+from data_process.ir_process import preprocess_transmittances_spectra_higer_500
+from data_process.ir_process import preprocess_transmittances_spectra_lower_500
 
+from test_and_infer.infer import ModelInference
+from test_and_infer.infer import get_feature_from_smiles
+
+# 100_Million_Scale_libray
 FEATURE_DIM = 1024
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -66,10 +76,77 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
 ]
 
+# unknown_data
 unknown_data_to_test = ["ir_path": os.path.join(PROJECT_ROOT,'data','unknown_data','100-Million-library-Retrieval','example_unknown_ir.jdx'),
              "MW_path": os.path.join(PROJECT_ROOT,'data','unknown_data','100-Million-library-Retrieval','example_unknown_MW.jdx'),
              "Formula_path": os.path.join(PROJECT_ROOT,'data','unknown_data','100-Million-library-Retrieval','example_unknown_formula.jdx'),
 ]
+
+# model
+'''
+You need to download the model weight file in hugging_face and save it in the check_points folder.
+'''
+TOKENIZER_PATH = os.path.join(PROJECT_ROOT,'model',"tokenizer-smiles-roberta-1e_new")
+PRETRAIN_SMILES_MODEL_PATH = os.path.join(PROJECT_ROOT, "check_points", "best_smiles_model_0.9230379746835443.pth")
+PRETRAIN_IR_MODEL_PATH = os.path.join(PROJECT_ROOT, "check_points", "best_ir_model_0.9230379746835443.pth")
+
+IR_model = IRModel()
+SmilesModel = SmilesModel(roberta_model_path=None,
+    roberta_tokenizer_path= TOKENIZER_PATH,
+    smiles_maxlen=300,
+    max_position_embeddings=505,
+    vocab_size=181,
+    feature_dim=768,
+)
+
+IR_model.to(device)
+SmilesModel.to(device)
+
+ModelInferenc = ModelInference(
+        SmilesModel,
+        IR_model,
+        pretrain_model_path_sm= PRETRAIN_SMILES_MODEL_PATH,
+        pretrain_model_path_ir= PRETRAIN_IR_MODEL_PATH,
+        device=None
+    )
+
+# process unknown ir
+def process_ir(ir_spectra_file, spectrum_type):
+    if hasattr(ir_spectra_file, 'name'):
+        file_path = ir_spectra_file.name
+    else:
+        file_path = ir_spectra_file
+    print(f"Processing file: {file_path}")
+    if file_path.lower().endswith('.csv'):
+        df = pd.read_csv(file_path)
+        wavenumbers = df.iloc[:, 0].values
+        transmittances = df.iloc[:, 1].values
+    elif file_path.lower().endswith('.jdx'):
+        data = jcamp.jcamp_readfile(file_path)
+        wavenumbers = np.array(data['x'], dtype=float)
+        transmittances = np.array(data['y'], dtype=float)
+    else:
+        raise ValueError("Unsupported file format. Please upload a CSV or JDX file.")
+    if spectrum_type == "absorbance spectrum":
+        if wavenumbers[0] > 500:
+            ir_data = preprocess_absorbances_spectra_higer_500(wavenumbers, transmittances)
+        else:
+            ir_data = preprocess_absorbances_spectra_lower_500(wavenumbers, transmittances)
+    else:
+        if wavenumbers[0] > 500:
+            ir_data = preprocess_transmittances_spectra_higer_500(wavenumbers, transmittances)
+        else:
+            ir_data = preprocess_transmittances_spectra_lower_500(wavenumbers, transmittances)
+    ir_spectra_tensor = torch.tensor(ir_data, dtype=torch.float32).unsqueeze(0).to(device)
+    with torch.no_grad():
+        ir_feature = ModelInferenc.ir_encode(ir_spectra_tensor)
+    return ir_feature
+    
+def load_MW_Formula(path):
+    with open(path, 'r') as f:
+        MW_or_Formula = f.read().splitlines()
+    return MW_or_Formula
+
 
 
 
